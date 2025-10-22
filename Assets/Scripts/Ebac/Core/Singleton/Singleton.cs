addcf2030be62b56cafd0b3876f36a443f12373c
Sylver-V <vicjsanches@gmail.com>
2025-10-22 14:20:00 -0300

    Criando GameManager e arrumando o StateMachine

---
+++ Assets/Scripts/Ebac/Core/Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ebac.Core.Singleton
+{
+
+
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance;
+
+        protected virtual void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this as T;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/Ebac/StateMachine/StateMachine.cs
+++ Assets/Scripts/Ebac/StateMachine/StateMachine.cs
@@ -2,56 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
+using JetBrains.Annotations;
 
-public class StateMachine : MonoBehaviour
+
+namespace Ebac.StateMachine
 {
-    public enum States
-    {
-        NONE,
-    }
-
-    public Dictionary<States, StateBase> dictionaryState;
-
-    private StateBase _currentState;
-    public float timeToStartGame = 1f;
-
-    private void Awake()
-    {
-        dictionaryState = new Dictionary<States, StateBase>();
-        dictionaryState.Add(States.NONE, new StateBase());
-
-        SwitchState(States.NONE);
-
-        Invoke(nameof(StartGame), timeToStartGame);
-    }
-
-    [Button]
-    private void StartGame()
-    {
-        //SwitchState(States.RUNNING);
-    }
-
-    [Button]
-    public void Aa (float a)
+    public class StateMachine<T> where T : System.Enum
     {
 
-    }
+        public Dictionary<T, StateBase> dictionaryState;
 
-    private void SwitchState(States state)
-    {
-        if (_currentState != null) _currentState.OnStateExit();
+        private StateBase _currentState;
+        public float timeToStartGame = 1f;
 
-        _currentState = dictionaryState[state];
-    }
-
-    public void Update()
-    {
-        if(_currentState != null) _currentState.OnStateStay();
-
-        if (Input.GetKeyDown(KeyCode.O))
+        public StateBase CurrentState
         {
-            //SwitchState(States.DEAD);
+            get { return _currentState; }
         }
-    }
 
+
+        public void Init()
+        {
+            dictionaryState = new Dictionary<T, StateBase>();
+        }
+
+        public void RegisterStates(T typeEnum, StateBase state)
+        {
+            dictionaryState.Add(typeEnum, state);
+        }
+
+
+        public void SwitchState(T state)
+        {
+            if (_currentState != null) _currentState.OnStateExit();
+
+            _currentState = dictionaryState[state];
+        }
+
+        public void Update()
+        {
+            if (_currentState != null) _currentState.OnStateStay();
+        }
+
+    }
 }
